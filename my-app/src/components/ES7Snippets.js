db7adb9dd46c01457297e3fc397b37b0fd4cf176
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-05 14:34:29 +0300

    Update

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,53 @@
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+//__________________________________________
+
+
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+//___________________________________________
+
+
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+//______________________________________________
+
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+//___________________________________________
+
